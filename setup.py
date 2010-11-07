a6cb636f9fd3b4e0190fb3a813d675474fa1c282
Frank DENIS <github@pureftpd.org>
2010-11-08 00:35:00 +0100

    Untab

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #! /usr/bin/env python
 
 try:
-	from setuptools import setup
-	
+    from setuptools import setup
+    
 except ImportError:
-	from distutils.core import setup
-	
-config = {
+    from distutils.core import setup
+    
+CONFIG = {
     "description": "Simple Comet",
-	"version": 0.1,
-	"license": "BSD",
-	"author": "Frank Denis",
-	"author_email": "j at pureftpd dot org",	
-	"url": "https://github.com/jedisct1/Simple-Comet-Server",
-	"download_url": "https://github.com/jedisct1/Simple-Comet-Server",
-	"install_requires": [ "nose", "twisted" ],
-	"packages": [ "simple_comet" ],
-	"scripts": [ "bin/simple-comet" ],
-	"name": "simple_comet"
+    "version": 0.1,
+    "license": "BSD",
+    "author": "Frank Denis",
+    "author_email": "j at pureftpd dot org",    
+    "url": "https://github.com/jedisct1/Simple-Comet-Server",
+    "download_url": "https://github.com/jedisct1/Simple-Comet-Server",
+    "install_requires": [ "nose", "twisted" ],
+    "packages": [ "simple_comet" ],
+    "scripts": [ "bin/simple-comet" ],
+    "name": "simple_comet"
 }
 
-setup(**config)
+setup(**CONFIG)
