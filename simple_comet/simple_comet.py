bb67cc83aace1f0e86ea81a1b0390f80ff82f037
Frank DENIS <github@pureftpd.org>
2010-11-13 12:14:24 +0100

    Fixups

--- simple_comet/simple_comet.py
+++ simple_comet/simple_comet.py
@@ -1,20 +1,18 @@
 
-def _install_reactors():
-    reactors_names = [ "kqreactor", "epollreactor", "pollreactor" ]
-    for reactor_name in reactors_names:
-        try:
-            exec("from twisted.internet import %s as _reactor" % reactor_name)
-            _reactor.install()
-            break
+from config import Config
+from twisted.web import server
+
+reactors_names = [ "kqreactor", "epollreactor", "pollreactor" ]
+for reactor_name in reactors_names:
+    try:
+        exec("from twisted.internet import %s as _reactor" % reactor_name)
+        _reactor.install()
+        break
         except AssertionError:
             break
         except Exception:
             pass
         
-
-from config import Config
-from twisted.web import server
-_install_reactors()
 from twisted.internet import reactor
 from comet_server import CometServer
 import sys
@@ -23,7 +21,6 @@ import sys
 def main():
     args = sys.argv[1: ]
     config = Config(args)
-    _install_reactors()
     comet_server = CometServer(config)
     site = server.Site(comet_server, timeout = config.http_timeout)
     reactor.listenTCP(config.http_port, site)
