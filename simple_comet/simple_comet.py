7b31b3a3ef571f1533b4998541a66ef1e1e109c5
Frank DENIS <github@pureftpd.org>
2010-11-13 11:49:56 +0100

    Install kqueue/epoll reactors. CF reactor seems broken on OSX 10.6.5 for now

--- simple_comet/simple_comet.py
+++ simple_comet/simple_comet.py
@@ -1,13 +1,28 @@
 
 from config import Config
 from twisted.internet import reactor
+
+        
+
 from twisted.web import server
 from comet_server import CometServer
 import sys
 
+def _install_reactors():
+    reactors = [ "kqreactor", "epollreactor", "pollreactor" ]
+    for reactor in reactors:
+        try:
+            exec("from twisted.internet import %s as _reactor" % reactor)
+            _reactor.install()
+            break
+        except Exception:
+            pass
+
+
 def main():
     args = sys.argv[1: ]
     config = Config(args)
+    _install_reactors()
     comet_server = CometServer(config)
     site = server.Site(comet_server, timeout = config.http_timeout)
     reactor.listenTCP(config.http_port, site)
