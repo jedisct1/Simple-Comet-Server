2951ab8ef9e5517372aee08853068aebcdfa22f5
Frank Denis <github@pureftpd.org>
2021-03-21 18:56:41 +0100

    Format

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 
 try:
     from setuptools import setup
-    
+
 except ImportError:
     from distutils.core import setup
-    
+
 CONFIG = {
     "description": "A simple Comet server",
     "version": 0.3,
     "license": "BSD",
     "author": "Frank Denis",
-    "author_email": "j at pureftpd dot org",    
+    "author_email": "j at pureftpd dot org",
     "url": "https://github.com/jedisct1/Simple-Comet-Server",
     "download_url": "https://github.com/jedisct1/Simple-Comet-Server",
-    "install_requires": [ "nose", "twisted", "simplejson" ],
-    "packages": [ "simple_comet" ],
-    "scripts": [ "bin/simple_comet" ],
+    "install_requires": ["nose", "twisted", "simplejson"],
+    "packages": ["simple_comet"],
+    "scripts": ["bin/simple_comet"],
     "name": "simple_comet"
 }
 
--- tests/simple-comet_tests.py
+++ tests/simple-comet_tests.py
@@ -19,69 +19,69 @@ def teardown():
     pass
 
 
-def _issue_command(uri_part, data, cb, method = None):
+def _issue_command(uri_part, data, cb, method=None):
     if data:
         if not method:
             method = "POST"
 
-        _data = urllib.urlencode(data)            
-        request = urllib2.Request(url = BASE_URI + uri_part, data = _data)
+        _data = urllib.urlencode(data)
+        request = urllib2.Request(url=BASE_URI + uri_part, data=_data)
         request.add_header("Content-Type", "application/x-www-form-urlencoded")
     else:
         if not method:
             method = "GET"
 
-        request = urllib2.Request(url = BASE_URI + uri_part)
-    
-    request.get_method = lambda: method        
+        request = urllib2.Request(url=BASE_URI + uri_part)
+
+    request.get_method = lambda: method
     st = urllib2.urlopen(request)
     res = json.loads(st.read())
     print(res)
     cb(res)
-    st.close()    
+    st.close()
 
 
 def test_register_channel():
     def _(res):
         assert(res["return_code"] > 0)
         assert(res["channel_id"] == "chan")
-        
-    _issue_command("/channels.json", { "channel_id": "chan" }, _)
-    
+
+    _issue_command("/channels.json", {"channel_id": "chan"}, _)
+
 
 def test_register_channel_with_sessions():
     def _(res):
         assert(res["return_code"] > 0)
         assert(res["channel_id"] == "chan-sess")
-        
+
     _issue_command("/channels.json",
-        { "channel_id": "chan-sess", "use_sessions": 1 }, _)
-                
-        
+                   {"channel_id": "chan-sess", "use_sessions": 1}, _)
+
+
 def test_push_content():
     def _(res):
         assert(res["return_code"] > 0)
-        
+
     _issue_command("/channels/chan.json",
-        { "content": "test" }, _)
-        
-        
+                   {"content": "test"}, _)
+
+
 def test_read_content():
     def _(res):
         assert(res["return_code"] > 0)
         assert(res["since"] == 0)
-        
+
     _issue_command("/channels/chan.json", None, _)
-    
-    
+
+
 def test_push_sess_content():
     def _(res):
         assert(res["return_code"] > 0)
-        
+
     _issue_command("/channels/chan-sess.json",
-        { "content": "test-sess" }, _)
-        
-        
+                   {"content": "test-sess"}, _)
+
+
 def test_read_unbound_sess_content():
     def _(res):
         assert(res["return_code"] < 0)
@@ -94,7 +94,7 @@ def test_register_client_id():
         assert(res["return_code"] > 0)
         assert(res["client_id"] == "cid")
 
-    _issue_command("/clients.json", { "client_id": "cid" }, _)
+    _issue_command("/clients.json", {"client_id": "cid"}, _)
 
 
 def test_read_sess_content():
@@ -110,12 +110,12 @@ def test_delete_chan():
         assert(res["removed"] == True)
 
     _issue_command("/channels/chan.json", None, _, "DELETE")
-    _issue_command("/channels/chan-sess.json", None, _, "DELETE")    
+    _issue_command("/channels/chan-sess.json", None, _, "DELETE")
 
 
 def test_delete_nonexistent_chan():
     def _(res):
         assert(res["return_code"] > 0)
-        assert(res["removed"] == False)        
+        assert(res["removed"] == False)
 
     _issue_command("/channels/nonexistent.json", None, _, "DELETE")
